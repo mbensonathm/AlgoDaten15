ddc6f57a2959b24b2dd3cefe093222db9563a232
Paul Seer <pseer@hm.edu>
2015-03-23 01:30:50 +0100

    Stringconding class, caption 16

--- src/actionsPackage/StringCoding.java
+++ src/actionsPackage/StringCoding.java
@@ -1,5 +1,30 @@
 package actionsPackage;
 
 public class StringCoding implements IActionsAtInsert{
+	
+	/* zählt */
+	private int counter = 0;
+	
+	public StringCoding(int start) {
+		this.counter = start;
+	}
+	
+	public void  setActualValue(int updateValue){
+		this.counter = updateValue;
+	}
+	
+	@Override
+	public Object actionAtKeyNotFound() {
+		return new Integer(counter++);
+	}
+
+	@Override
+	public Object actionAtKeyFound(Object previous) {
+		return previous;
+	}
+	
+	public int getActualValue(){
+		return this.counter;
+	}
 
 }
