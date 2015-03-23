fe05a780e8fb8e7bef93eb7fa9688b4f9398bd1c
zeroeightnine <vfrank@live.de>
2015-03-23 13:10:58 +0100

    Vitos Test Commit

--- src/triePackage/ITrie.java
+++ src/triePackage/ITrie.java
@@ -3,8 +3,14 @@ package triePackage;
 import java.util.Iterator;
 import actionsPackage.IActionsAtInsert;
 
+/**
+ * test
+ *
+ * @author V
+ *
+ */
 public interface ITrie {
-	
+
 	public void insert(Iterator iterator, IActionsAtInsert actionAtInsert);
 	public void insert(String string, IActionsAtInsert actionAtInsert);
 
