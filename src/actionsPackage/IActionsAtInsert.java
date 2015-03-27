f6ad88478c512701fb22326e81986f6e313afe0e
Mark Benson <benson@hm.edu>
2015-03-27 14:07:17 +0100

    TreeNode

--- src/actionsPackage/IActionsAtInsert.java
+++ src/actionsPackage/IActionsAtInsert.java
@@ -4,5 +4,7 @@ public interface IActionsAtInsert {
 
 	public Object actionAtKeyNotFound();
 	public Object actionAtKeyFound(Object previous);
+	public void setActualValue(Object value);
+	public Object getActualValue();
 	
 }
--- src/triePackage/TreeNode.java
+++ src/triePackage/TreeNode.java
@@ -1,11 +1,23 @@
 package triePackage;
 
 import java.util.Iterator;
+import java.util.Map;
 
+import mapPackage.IMapFactory;
 import actionsPackage.IActionsAtInsert;
 
 public class TreeNode implements ITrieNode{
-
+	final private Map outgoingEdgeMap;
+	final private ITrieNode parent;
+	final private Comparable incomingPartialKey;
+	final private IMapFactory mapFactory;
+	
+	public TreeNode(IMapFactory mapFactory, ITrieNode parent, Comparable incomingPartialKey){
+		this.mapFactory = mapFactory;
+		this.parent = parent;
+		this.incomingPartialKey = incomingPartialKey;
+		this.outgoingEdgeMap = this.mapFactory.create();
+	}
 	@Override
 	public ITrieReference recursivInsert(Iterator iterator,
 			IActionsAtInsert actionAtInsert) {
